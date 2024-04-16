fe7a8a4bf574baae5bc92e138aa79ba3b9b3a2fd
tpdl9612 <tpdl9612@gmail.com>
2024-04-16 16:55:59 +0900

    initial ValueHomeWork

---
+++ src/main/java/com/korea/JpaMission1/JpaMission1Application.java
@@ -0,0 +1,13 @@
+package com.korea.JpaMission1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMission1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMission1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/korea/JpaMission1/JpaMission1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korea.JpaMission1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMission1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
